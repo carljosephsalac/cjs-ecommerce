7089057985e0136bbb1b04880d0c48ff8cc22f75
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-06-10 19:48:24 +0800

    feat(js): use products data from separate file

--- javascripts/cjs-ecommerce.js
+++ javascripts/cjs-ecommerce.js
@@ -1,4 +1,5 @@
-const products = [{
+// how to create products data
+const cjsProducts = [{
   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
   rating: {
@@ -22,6 +23,14 @@ const products = [{
     count: 56
   },
   priceCents: 799
+}, {
+  image: 'images/products/black-2-slot-toaster.jpg',
+  name: '2 Slot Toaster - Black',
+  rating: {
+    stars: 5,
+    count: 2197
+  },
+  priceCents: 1899
 }];
 
 let productsHTML = '';
