1e12a587dbb0bd077e84232f9c894aafc16af3c0
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-06-10 19:30:11 +0800

    feat(js): use JavaScript to generate HTML

---
+++ javascripts/cjs-ecommerce.js
@@ -0,0 +1,58 @@
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090, // save in cents
+}, {
+  image: 'images/products/intermediate-composite-basketball.jpg',
+  name: 'Intermediate Size Basketball',
+  rating: {
+    stars: 4,
+    count: 127
+  },
+  priceCents: 2095
+}, {
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name: 'Adult Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799
+}];
+
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img src="${product.image}" class="product-image">
+      </div>
+
+      <div class="product-name">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10 /*convert to whole number*/}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2) /*convert to decimal(fixed to 2 places)*/}
+      </div>
+    </div>
+  `;   
+});
+
+const productsGrid = document.querySelector('.js-products-grid');
+productsGrid.innerHTML = productsHTML;
+
+
