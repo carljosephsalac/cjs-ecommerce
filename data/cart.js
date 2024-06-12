80f123f47a23926451ff2db0cf8d9da4550030a8
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-06-12 16:42:51 +0800

    feat(js): delete products form HTML when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -38,6 +38,5 @@ export function removeFromCart (productId) {
       newCart.push(cartItem);
     }
   });
-
   cart = newCart;
 }
--- javascripts/checkout.js
+++ javascripts/checkout.js
@@ -10,7 +10,7 @@ cart.forEach((item) => {
     if (item.productId === product.id) {
       matchingProduct = product;
       cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
           <div class="delivery-date">
             Delivery date: Tuesday, June 21
           </div>
@@ -90,5 +90,7 @@ document.querySelectorAll('.js-delete-link').forEach((deleteLink) => {
   deleteLink.addEventListener('click', () => {
     const productId = deleteLink.dataset.productId;    
     removeFromCart(productId);    
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
