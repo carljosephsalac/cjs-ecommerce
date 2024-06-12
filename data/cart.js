1547d2666a83f0b968fbcc4d67d1af4d3ce48204
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-06-12 21:03:11 +0800

    feat(js): save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,6 @@
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 1
-}, {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}, {
-  productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  quantity: 2
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-export function addToCart (productId, productName) {
+export function addToCart(productId, productName) {
   const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);             
   let matchingItem;    
 
@@ -29,9 +20,10 @@ export function addToCart (productId, productName) {
       quantity: Number(quantitySelector.value)
     });
   }    
+  saveToStorage();
 }
 
-export function removeFromCart (productId) {
+export function removeFromCart(productId) {
   const newCart = [];
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
@@ -39,4 +31,9 @@ export function removeFromCart (productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
