7a2c62ef46d86057edb0c4f25f7d0ad0198725a0
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-06-17 23:15:12 +0800

    feat(js): create order using the backend

--- javascripts/checkout/paymentSummary.js
+++ javascripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart, calculateCartQuantity } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import toCents from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
   /* MODEL */
@@ -48,11 +49,32 @@ export function renderPaymentSummary() {
       <div class="payment-summary-money">$${toCents(orderTotalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="js-place-order place-order-button button-primary">
       Place your order
     </button>
   `
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      })
+      const order = await response.json();
+      addOrder(order);
+
+    } catch (error) {
+      console.log(error);
+    }
+    
+    window.location.href = 'orders.html';
+  });
   
   // console.log(`Items: $${toCents(productPriceCents)}`);
   // console.log(`Shipping and handling: $${toCents(shippingPriceCents)}`);
